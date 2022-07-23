9a79bdd773d165879d1d78d32c985dea9d51e12b
Ariel <wany468@aucklanduni.ac.nz>
2022-07-23 16:21:40 +1200

    test timer functionality

--- content-script.js
+++ content-script.js
@@ -1,7 +1,37 @@
 let playButton = document.getElementById("playButton");
+let playButtonClicksCounter = 0;
+let interval;
+let secondsElapsed;
+let playStatus = true;
+let timer = 0;
+playButton.addEventListener("click", playOrPause);
 
-playButton.addEventListener("click", timer);
+/* playButton.addEventListener("click", timer);
 
 function timer() {
+    playButtonClicksCounter++;
+    interval = setInterval(countTime, 1000);
+
     console.log("hihi");
+}
+
+function countTime() {
+    secondsElapsed++;
+    console.log(secondsElapsed);
+} */
+
+
+function playOrPause() {
+    playStatus = !playStatus;
+    if (playStatus) {
+        //increment timer every second
+        interval = setInterval(function () {
+            console.log("ouio");
+            secondsElapsed++;
+        }, 1000);
+
+    } else {
+        clearInterval(interval);
+    }
+
 }
