c18a8020a35fcf44f6de0dc899d6948170af6925
tnai255 <tnai55@aucklanduni.ac.nz>
2022-07-23 19:40:53 +1200

    On click added items.

--- script.js
+++ script.js
@@ -1,3 +1,72 @@
 // JavaScript source code
-// console.log("hello" + document.getElementById("hist_frame"));
-// alert(document.getElementById("hist_frame"));
+
+window.onload = function () {
+    document.getElementById("cowboyhatbtn").onclick = function show() {
+    
+        var x = document.getElementById('cowboyhat');
+        if (x.style.display === "none") {
+            x.style.display = "block";
+         } else {
+            x.style.display = "none";
+        }
+        
+    };
+
+    document.getElementById("beaniebtn").onclick = function show() {
+    
+        var x = document.getElementById('beanie');
+        if (x.style.display === "none") {
+            x.style.display = "block";
+         } else {
+            x.style.display = "none";
+        }
+        
+    };
+
+    document.getElementById("bowbtn").onclick = function show() {
+    
+        var x = document.getElementById('bow');
+        if (x.style.display === "none") {
+            x.style.display = "block";
+         } else {
+            x.style.display = "none";
+        }
+        
+    };
+
+    document.getElementById("bunnybtn").onclick = function show() {
+    
+        var x = document.getElementById('bunny');
+        if (x.style.display === "none") {
+            x.style.display = "block";
+         } else {
+            x.style.display = "none";
+        }
+        
+    };
+
+    document.getElementById("necklacebtn").onclick = function show() {
+    
+        var x = document.getElementById('necklace');
+        if (x.style.display === "none") {
+            x.style.display = "block";
+         } else {
+            x.style.display = "none";
+        }
+        
+    };
+
+    document.getElementById("sockbtn").onclick = function show() {
+    
+        var x = document.getElementById('sock');
+        if (x.style.display === "none") {
+            x.style.display = "block";
+         } else {
+            x.style.display = "none";
+        }
+        
+    };
+}
+
+
+
