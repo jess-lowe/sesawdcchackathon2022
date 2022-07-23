2ca365042f73c6ea2a2298d00070e53108a4319b
boxy8 <zou.emily@yahoo.co.nz>
2022-07-24 00:44:01 +1200

    Implement hide show functionality on wardrobe items

--- content-script.js
+++ content-script.js
@@ -1,32 +1,45 @@
-// initialise points
-let newPoints = 0;
-let storedPoints;
+let newSeconds = 0;
+let pointsFactor = 1;  // calculation: total seconds / pointsFactor = points
 
+// if no existing points exist, set storedPoints to be 0
 if (localStorage.getItem('storedPoints') == null) {
     localStorage.setItem('storedPoints', "0");
 }
 
-let interval;
-
 // get primary video player element
 let video = document.getElementById("primaryVideo");
 
-video.onplay = (event) => {
-    interval = setInterval(function () {
-        console.log(newPoints);
-        newPoints++;
+
+// interval for points counter
+let counterInterval;
+
+// count points every second while lecture plays
+video.onplay = () => {
+    counterInterval = setInterval(() => {
+        console.log(newSeconds);
+        newSeconds++;
     }, 1000);
 }
 
-video.onpause = (event) => {
-    clearInterval(interval);
+
+// clear counting interval if lecture paused
+video.onpause = () => {
+    clearInterval(counterInterval);
 }
 
+// interval for storing points every 10s
 let storePointsInterval = setInterval(storePoints, 10000);
 
+// update (int) storedPoints by adding new points accumulated since last update
 function storePoints() {
-    storedPoints = newPoints + parseInt(localStorage.getItem('storedPoints'));
-    localStorage.setItem('storedPoints', storedPoints.toString());
-    newPoints = 0;
+    let newPoints = Math.round(newSeconds / pointsFactor);  // calculate (rounded) new points accumulated
+
+    // if at least 1 accumulated, update storage and reset newSeconds
+    if (newPoints > 0) {
+        let storedPoints = newPoints + parseInt(localStorage.getItem('storedPoints'));
+        localStorage.setItem('storedPoints', storedPoints.toString());
+        newSeconds = 0;
+        // call update points display
+    }
 }
 
--- script.js
+++ script.js
@@ -1,72 +1,128 @@
 // JavaScript source code
 
-window.onload = function () {
-    document.getElementById("cowboyhatbtn").onclick = function show() {
-    
-        var x = document.getElementById('cowboyhat');
-        if (x.style.display === "none") {
-            x.style.display = "block";
-         } else {
-            x.style.display = "none";
-        }
-        
-    };
+// NB: js "style" attribute != css file - use class instead for e.g. ".lock" 
 
-    document.getElementById("beaniebtn").onclick = function show() {
-    
-        var x = document.getElementById('beanie');
-        if (x.style.display === "none") {
-            x.style.display = "block";
-         } else {
-            x.style.display = "none";
-        }
-        
-    };
+// // if no item
+// if (localStorage.getItem('storedPoints') == null) {
+//     localStorage.setItem('storedPoints', "0");
+// }
 
-    document.getElementById("bowbtn").onclick = function show() {
-    
-        var x = document.getElementById('bow');
-        if (x.style.display === "none") {
-            x.style.display = "block";
-         } else {
-            x.style.display = "none";
-        }
-        
-    };
+// unhardcode this tmr
+let wardrobePointUnlocks = {
+    "cowboyhatbtn": 1000,
+    "beaniebtn": 2000,
+    "bowbtn": 3000,
+    "bunnybtn": 4000,
+    "necklacebtn": 5000,
+    "sockbtn": 6000,
+};
 
-    document.getElementById("bunnybtn").onclick = function show() {
-    
-        var x = document.getElementById('bunny');
-        if (x.style.display === "none") {
-            x.style.display = "block";
-         } else {
-            x.style.display = "none";
-        }
-        
-    };
+window.onload = function() {
+    for (const itemId in wardrobePointUnlocks) {
+        document.getElementById(itemId).onclick = () => {
+            if (document.getElementById(itemId).classList.contains("locked")) {
+                let points = localStorage.getItem('storedPoints');
+                console.log("POOR");
+            }
 
-    document.getElementById("necklacebtn").onclick = function show() {
-    
-        var x = document.getElementById('necklace');
-        if (x.style.display === "none") {
-            x.style.display = "block";
-         } else {
-            x.style.display = "none";
+            toggleWear(document.getElementById(itemId.slice(0, -3)));
         }
-        
-    };
+    }
 
-    document.getElementById("sockbtn").onclick = function show() {
+//     // listen for clicks on wardrobe div
+// document.getElementById("wardrobe").onclick = (event) => {
+
+//     // if click wasn't on a grid item, return
+//     console.log(event.target.classList);
+//     if (!(event.target.tagName == "BUTTON" || event.target.tagName == "IMG")) {
+//         return;
+//     }
+
+//     // if item is currently locked
+//     if (event.target.classList.contains("locked")) {
+//         console.log("LOCKED UR POOR AF");
+//         // check if we can unlock it
+//         // if we can then remove class, store result in local storage
+//         // otherwise return i guess...
+//     }
+//     // toggleWear(event.target.classList.contains("hidden"));
+// };
+
+// // document.getElementById("cowboyhatbtn").onclick = (event) => {
+// //     // if item is currently locked
+// //     if (event.target.classList.includes("locked")) {
+// //         // check if we can unlock it
+// //         // if we can then remove class, store result in local storage
+// //         // otherwise return i guess...
+// //     }
+// //     toggleWear(event.target.classList.includes("hidden"));
+// // };
+
+// // document.getElementById("beaniebtn").onclick = function show() {
+
+// //     var x = document.getElementById('beanie');
+// //     console.log("HELLO")
+// //     if (x.style.display === "none") {
+// //         x.style.display = "block";
+// //         } else {
+// //         x.style.display = "none";
+// //     }
     
-        var x = document.getElementById('sock');
-        if (x.style.display === "none") {
-            x.style.display = "block";
-         } else {
-            x.style.display = "none";
+// // };
+
+// // document.getElementById("bowbtn").onclick = function show() {
+
+// //     var x = document.getElementById('bow');
+// //     // if (x.style.display === "none") {
+// //     //     x.style.display = "block";
+// //     //     } else {
+// //     //     x.style.display = "none";
+// //     // }
+    
+// // };
+
+// // document.getElementById("bunnybtn").onclick = function show() {
+
+// //     var x = document.getElementById('bunny');
+// //     // if (x.style.display === "none") {
+// //     //     x.style.display = "block";
+// //     //     } else {
+// //     //     x.style.display = "none";
+// //     // }
+    
+// // };
+
+// // document.getElementById("necklacebtn").onclick = function show() {
+
+// //     var x = document.getElementById('necklace');
+// //     // if (x.style.display === "none") {
+// //     //     x.style.display = "block";
+// //     //     } else {
+// //     //     x.style.display = "none";
+// //     // }
+    
+// // };
+
+// // document.getElementById("sockbtn").onclick = function show() {
+
+// //     var x = document.getElementById('sock');
+// //     // if (x.style.display === "none") {
+// //     //     x.style.display = "block";
+// //     //     } else {
+// //     //     x.style.display = "none";
+// //     // }
+    
+// // };
+
+
+    function toggleWear(element) {
+        if (element.classList.contains("hidden")) {
+            element.classList.remove("hidden");
+        } else {
+            element.classList.add("hidden")
         }
-        
-    };
+        console.log(element)
+    }
 }
-
 
 
